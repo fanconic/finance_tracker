6064f5cdb8fa33e26236986cabc8056e30790936
fanconic <fanconic@student.ethz.ch>
2020-04-21 20:05:49 +0200

    Added new features to stock predictor

---
+++ rule-based-algorithm/README.md
@@ -0,0 +1,6 @@
+# Rule-based Trading Algorithm
+Experiment of using moving averages to beat the S&P500 SPY index through backtesting
+
+Second step of the experimet would be to apply the same algorithm to beat the SMI, 
+by following the UBS ETF (CH) SMI (CHF) A-dis
+"""
--- stock_predictor.py
+++ stock_predictor.py
@@ -14,41 +14,84 @@ import pandas as pd
 import math
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler, scale
-from sklearn.model_selection import train_test_split as tts
+from sklearn.model_selection import TimeSeriesSplit
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVR
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import median_absolute_error
 
 tickerData = yf.Ticker("UBS")
-df = tickerData.history(period='1d', start="2015-1-1", end="2020-04-06")
-
+df = tickerData.history(period="1d", start="2015-1-1", end="2020-04-20")
 
 # Create some extra features
-df['hl_pct'] = (df['High'] - df['Close']) / df['Close']
-df['delta_pct'] = (df['Close'] - df['Open']) / df['Close']
+df["hl_pct"] = (df["High"] - df["Close"]) / df["Close"]
+df["delta_pct"] = (df["Close"] - df["Open"]) / df["Close"]
+
+# Moving Average (Weekly, Monthly, Quarterly)
+df["5d_uma"] = df["Close"].rolling(5).mean()
+df["20d_uma"] = df["Close"].rolling(20).mean()
+df["60d_uma"] = df["Close"].rolling(60).mean()
+
+# Exponential Moving Average
+df["5d_ema"] = df["Close"].ewm(span=5).mean()
+df["20d_ema"] = df["Close"].ewm(span=20).mean()
+df["60d_ema"] = df["Close"].ewm(span=60).mean()
 
 # Decide how many days ahead our algorithm should predict
-future_forecast = 10 # days into the future
+future_forecast = 1  # days into the future
 df["label"] = df["Close"].shift(-future_forecast)
 df.dropna(inplace=True)
 
 # Training data
-X = df.drop(["label"], axis = 1)
-X = scale(X)
+X = df.drop(["label"], axis=1)
+# X = pd.DataFrame(scale(X))
 y = df["label"]
 
-# Train-Test Split
-X_train, X_test, y_train, y_test = tts(X, y, test_size = 0.2, random_state= 42)
 
+test_size = 20
+X_test = X[-test_size:]
+y_test = y[-test_size:]
+X = X[:-test_size]
+y = y[:-test_size]
 
-# Regressor
-model = RandomForestRegressor(n_estimators=200, random_state=42)
-# Train model
-model.fit(X_train, y_train)
+# Time Series Split:
+tscv = TimeSeriesSplit(n_splits=10, max_train_size=None)
 
-# Asses model
-score  = model.score(X_test, y_test)
-print("R² score of Regressor: ", score)
+# Linear regression model
+model = LinearRegression()
 
-# Plot results
-df["Close"].plot()
+# Cross Validate Model
+for train_index, val_index in tscv.split(X):
+    X_train, X_val = X.iloc[train_index], X.iloc[val_index]
+    y_train, y_val = y.iloc[train_index], y.iloc[val_index]
+
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_val)
+
+    print("MAE: ", median_absolute_error(y_val, y_pred))
+    print("R² Value: ", model.score(X_val, y_val), "\n")
+
+# Train Model
+model.fit(X, y)
+y_pred = model.predict(X_test)
+print("MAE: ", median_absolute_error(y_test, y_pred))
+print("R² Value: ", model.score(X_test, y_test))
+
+# Plot Results
+preds = np.empty(len(df))
+preds[:] = np.nan
+preds[-test_size:] = y_pred
+df["preds"] = preds
+
+fig = plt.figure(figsize=(20, 10))
+plt.plot(df["Close"], label="reality")
+plt.plot(df["preds"], color="r", label="prediction")
+plt.legend()
+plt.show()
+
+# Show Zoom of prediction
+fig = plt.figure(figsize=(20, 10))
+plt.plot(df["Close"][-test_size:], label="reality")
+plt.plot(df["preds"][-test_size:], color="r", label="prediction")
+plt.legend()
+plt.show()
